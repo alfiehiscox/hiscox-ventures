96944b1c7633766088233237079b1606dda79c47
alfie hiscox <alfie.hiscox@gmail.com>
2021-01-28 17:22:06 +0000

    Playing around with some background ideas

--- src/components/expertise-header.jsx
+++ src/components/expertise-header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/expertise-header.scss';
-import { Title } from './tabs/TabsLarge';
 import ExpertiseCard from './cards/expertise-card';
 import Expertise from '../images/values-img/expertise.svg';
 import Results from '../images/values-img/results.svg';
@@ -11,14 +10,7 @@ const ExpertiseHeader = (props) => {
     <div>
       <div className="nav-bar-cover"></div>
       <div className="header-background">
-        <Title 
-          title="Our Expertise"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                       Fusce blandit euismod nisl a vulputate.Lorem ipsum dolor 
-                       sit amet, consectetur adipiscing elit. 
-                       Fusce blandit euismod nisl a vulputate"
-          color="white"
-        />
+        <h1 className="expertise-header-title">Our Expertise</h1>
         <div className="outer-expertise-card-container">
           <ExpertiseCard 
             image={Expertise}
@@ -48,6 +40,12 @@ const ExpertiseHeader = (props) => {
             itemNum=" item-3"
           />
         </div>
+        <svg className="expertise-wavy-svg" width="1440" height="380" viewBox="0 0 1440 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 380H1440V160.264C1347 184.783 1243.52 1.86441 1112.38 0.02063C961.414 -2.10189 877.509 
+          160.264 726.5 160.264C575.491 160.264 494.323 -1.42032 343.333 0.02063C206.406 1.32739 108 182.42 0 
+          160.264V380Z" fill="white"/>
+        </svg>
+
       </div>
     </div>
   );
